f8f092303399b2c869814c083f95057395e99e30
eight <eight04@gmail.com>
2018-05-06 07:07:02 +0800

    Fix: typo

--- cli.js
+++ cli.js
@@ -5,8 +5,8 @@ Usage:
   start-browser <target> [-- <args>...]
   
 Options:
-  <target>  A file or a URL. If the file doesn't exists, the tool assumes the
-            target is a URL.
+  <target>  A file or a URL. If the file doesn't exist, the target is treated
+            as a URL.
   <args>    Other arguments for the browser.
 `;
 require(".").init(run(docString));
