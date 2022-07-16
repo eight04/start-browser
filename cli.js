e07b55576fda014d918bcbd6233f0b240615878f
eight <eight04@gmail.com>
2022-07-16 17:15:54 +0800

    Fix: failed starting Firefox (#1)
    * Fix: failed starting Firefox
    
    * Add: action
    
    * Fix: linter

--- cli.js
+++ cli.js
@@ -4,9 +4,10 @@ const {run} = require("neodoc");
 const docString = `start-browser
 
 Usage:
-  start-browser <target> [-- <args>...]
+  start-browser [--verbose] <target> [-- <args>...]
   
 Options:
+  --verbose, -V  Show verbose information.
   <target>  A file or a URL. If the file doesn't exist, the target is treated
             as a URL.
   <args>    Other arguments for the browser.
