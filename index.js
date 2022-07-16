a4a10493290f4761c750115a2acc2ccea0d37aa0
eight04 <eight04@gmail.com>
2022-07-16 17:03:18 +0800

    Fix: failed starting Firefox

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const {promisify} = require("util");
 const os = require("os");
 const access = promisify(require("fs").access);
-const {spawn} = require("child_process");
 
 const getDefaultBrowser = promisify(require("x-default-browser"));
 const fileUrl = require("file-url");
 
-function getCommand() {
+function getCommand(verbose) {
   return getDefaultBrowser()
     .then(result => {
+      if (verbose) {
+        console.log(result);
+      }
       // windows
       if (os.platform() === "win32") {
-        const path = `HKLM\\Software\\Clients\\StartMenuInternet\\${result.identity}\\shell\\open\\command`;
+        const identity = result.identity.replace(/firefoxurl/i, "Firefox")
+        const path = `HKLM\\Software\\Clients\\StartMenuInternet\\${identity}\\shell\\open\\command`;
         return Promise.resolve(require("regedit"))
           .then(regedit => promisify(regedit.list)(path))
           .then(result => {
@@ -31,23 +34,26 @@ function getURL(file) {
     );
 }
 
-function init({
+async function init({
+  "--verbose": verbose,
   "<target>": target,
   "--": args
 }) {
-  return Promise.all([getCommand(), getURL(target)])
-    .then(([command, url]) => {
-      if (/^(["']).*\1$/.test(command)) {
-        command = command.slice(1, -1);
-      }
-      const sp = spawn(command, [url].concat(args), {
-        stdio: "ignore",
-        detached: true,
-        shell: true,
-        windowsHide: true
-      });
-      sp.unref();
-    });
+  const execa = (await import("execa")).execa;
+  let [command, url] = await Promise.all([getCommand(verbose), getURL(target)]);
+  if (/^(["']).*\1$/.test(command)) {
+    command = command.slice(1, -1);
+  }
+  if (verbose) {
+    console.log("starting the browser");
+    console.log(command);
+    console.log(url, ...args);
+  }
+  const process = execa(command, [url, ...args], {detached: !verbose});
+  if (!verbose) {
+    process.unref();
+  }
+  await process;
 }
 
 module.exports = {init};
